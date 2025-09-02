43f7d27a5da058381591b0e9e2a41090c00329eb
naveenchittiphp <74538838+naveenchittiphp@users.noreply.github.com>
2025-09-02 08:58:19 +0530

    Create 102.UnderstandingLargeLanguageModelsLLMs.php

---
+++ 102.UnderstandingLargeLanguageModelsLLMs.php
@@ -0,0 +1,375 @@
+All right, so welcome to the very interesting section
+
+that is how LLM works.
+
+So this whole agentic AI,AI and all the buzzwords that you hear in the market are basically all around the LLM.
+
+So LLM stands for large Language model.
+
+So in this particular video, I'll keep this particular video a, very beginner friendly and let's just understand what is an LLM.
+
+In this particular section you're going to see what is
+
+an LLM, how LLMs internally work,
+
+what are vector embeddings,
+
+what is tokenization,
+
+what is attention is all you need.
+
+White paper by Google.
+
+So this is going to be a really,
+
+really, really brain opening,
+
+eye opening, videos section
+
+where you will understand that how
+
+internally the LLMs work.
+
+So with that let's start
+
+with the video.
+
+So I'm pretty sure that you
+
+have used ChatGPT, right?
+
+Or maybe Gemini or maybe
+
+Claude, any one of them.
+
+And you in your day to day activity,
+
+you like asking questions
+
+to these LLM models.
+
+Just for an example, let me just
+
+quickly do a screen sharing.
+
+So if you can see on my screen I have
+
+something very familiar opened here.
+
+That is a chart GPT.
+
+Now to this chart GPT what
+
+happens is when I say hi
+
+magically it replies me hey piyush,
+
+what can I help you with?
+
+If I ask is, hey, what is 2 plus 2?
+
+What you are going to notice is that
+
+you are going to get an answer that
+
+is two plus two equals to four.
+
+So it feels like that
+
+there is someone who is chatting
+
+with you, right?
+
+You are chatting with someone
+
+and as this website says
+
+that this is chat GPT.
+
+Chat GPT means that you are
+
+basically chatting with a GPT.
+
+And what is GPT?
+
+GPT is basically an LLM LLM okay,
+
+*** LLM stands for Large Language model.So if you ask any question to these large language models, they give
+you some output, right, based on how they have been trained.
+
+For example if I ask him
+
+what is, okay, what is an large,
+
+what is a large language model?
+
+And enter so what you are going
+
+to notice is that it is actually
+
+going to draft an answer for me.
+
+A large language model is
+
+an artificial intelligence system
+
+trained to understand
+
+number one, understand
+
+and generate human language.
+
+So these are some models, some
+
+systems which are trained
+
+to number one understand
+
+the natural language and to generate
+human language outputs.
+
+So right now whatever I'm speaking,
+
+you are able to hear me out.
+
+Why?
+
+Because you understand English
+
+language, you understand what I'm
+
+saying and based on that you are
+
+also able to respond to me.
+
+So these LLMs are basically
+
+an AI systems which are trained
+
+to do the same thing.
+
+So if you see they are
+
+already trained.
+
+So the Chat GPT, the company
+
+behind Chat GPT is OpenAI.
+
+So OpenAI has actually trained these
+
+models on whole Internet data.
+
+All right, like Your tweets,
+
+your LinkedIn posts, your
+
+Facebook post, all that is used
+
+in training of these data.
+
+So what are these tokens and all we
+
+will talk about this thing and what
+
+happens is now based on that
+
+training data, whenever you ask them
+
+something, they give you the output
+
+for that.
+
+So basically Chat GPT is nothing.
+
+GPT is the actual LLM,
+
+the large language model with
+
+which you are chatting.
+
+And chat GPT.
+
+GPT is not the only
+
+and only LLM in the world.
+
+There are even more LLMs.
+
+For example, example here you
+
+can see that this is a Gemini.
+
+Gemini is again a large
+
+language model by Google.
+
+So all these big tech giant companies
+
+are now playing on the data.
+
+They scrape the data, they get
+
+the data, they prepare something
+
+known as training data set and then
+
+they are training these LLMs.
+
+Okay, so to this LLM, I can ask, hey,
+
+what is an LLM
+
+that is in, in the context of AI.
+
+Okay, in the context of AI.
+
+And I just do an enter.
+
+So what you're going to notice is
+
+does something, right, based on
+
+whatever I have asked, it is now
+
+thinking, okay, it is now thinking
+
+something and this thinking and
+
+all goes into what is in the
+
+training data.
+
+And now quickly you are going
+
+to see some response.
+
+So yeah, it is going
+
+to take some time.
+
+So of course, here's
+
+a detailed explanation.
+
+So LLM stands for
+
+Large Language Model.
+
+At its core, an LLM is
+
+a sophisticated type of artificial
+
+intelligence AI that has been
+
+trained to understand, process
+
+and generate human language.
+
+So what you can understand
+
+is basically the human
+
+language is the main thing.
+
+Whenever we talk to systems,
+
+we always have to talk
+
+in a structured manner.
+
+Maybe you have to write a code in C,
+
+maybe you have to write a code in C.
+
+But using these LLMs, you
+
+can actually talk
+
+to the machine in a natural
+
+language like we humans do.
+
+So this is the main
+
+USP of these LLMs.
+
+So right now in the market,
+
+there are lots and lots of LLMs.
+
+There is OpenAI, there is GPT
+
+GPT 3.5 GPT 3.5 Turbo GPT 4 GPT 4O
+
+GPT GPT 03, mini GPT 03.
+
+And from Gemini you have a lot
+
+of models you can see here, right?
+
+The list is very big actually.
+
+2.5, 2.5 Pro.
+
+There are some preview models.
+
+There is a, 1.5 models.
+
+Then there is some Claude, Claude
+
+4 Claude Sonet, Claude 4 Pro.
+
+A lot and lots of LLMs are there.
+
+They are trained
+
+on different data sets.
+
+They have different capabilities,
+
+different speed.
+
+But at the end, the goal is
+
+to understand the natural language
+
+and generate the natural language,
+
+which is, useful for us.
+
+So this is the definition of LLM.
+
+I'm not telling you that this is
+
+the internal working of LLM.
+
+This is just the definition
+
+of an LLM.
+
+In the next video we are going to
+
+see how these LLMs internally work.
+
+We will see some in depth guide that.
+
+Okay, what happens when you
+
+say, hey, what happens now?
+
+How magically to this hey.
+
+This LLM responds
+
+with hey, again, push.
+
+So this is what we are going
+
+
+Important points :
+LLMS -> Large language models 
+Generally we have to write the code or structure langugeate to communcate with the system .
+But with LLMS we can communicate with human language . these are trained to understand the human language 
+
+to understand in the next video.
