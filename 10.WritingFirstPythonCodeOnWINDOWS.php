0598d3802b744aa4e4bd2b7e59d4f3296f6ab98e
naveenchittiphp <74538838+naveenchittiphp@users.noreply.github.com>
2025-09-02 08:48:25 +0530

    Create 10.WritingFirstPythonCodeOnWINDOWS.php

---
+++ 10.WritingFirstPythonCodeOnWINDOWS.php
@@ -0,0 +1,27 @@
+Installation : 
+Go to the website and download the python for the windows 
+https://www.python.org/
+Use admin privileges when installing py.exe
+Add python.exe to PATH
+Click on the both checkbox so we dont need to worry about setting the path.
+
+Now installl the warp for windows 
+url : https://www.warp.dev/
+
+Warp is a modern terminal emulator built with a focus on productivity and collaboration. It is designed to be a faster, more user-friendly alternative to traditional terminals.
+
+Extensions for VSCode :
+Python
+Microsoft
+microsoft.com
+181,366,551
+(618)
+Python language support with extension access points for IntelliSense (Pylance), Debugging (Python Debugger), linting, formatting
+
+pythontest.py
+-----------
+import sys -> importing system libraries
+
+print(sys.version)
+E:\python>python pythontest.py
+3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)]
